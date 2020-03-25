a8021299f7a898445f02916751ffb3cfe71ecf1f
bigf00t <jeremydsnell@gmail.com>
2020-03-24 17:13:17 -0700

    Added favicon and anlaytics, updated to firebase 7.12.0

---
+++ src/fire.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYdGDedLLPfXINfadAJiALOrmZLRaToH8",
+  authDomain: "bgpeen-1fc16.firebaseapp.com",
+  databaseURL: "https://bgpeen-1fc16.firebaseio.com",
+  projectId: "bgpeen-1fc16",
+  storageBucket: "bgpeen-1fc16.appspot.com",
+  messagingSenderId: "730875464009",
+  appId: "1:730875464009:web:32742d8a37ed4e84",
+  measurementId: "G-D31H7RXK79"
+}
+
+export const fire = firebase.initializeApp(firebaseConfig);
+fire.analytics();
